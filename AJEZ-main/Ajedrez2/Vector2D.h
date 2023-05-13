6a02dca12e9f285f025478bf081bd7e04959d20a
CarlosMiguelMartin <61314265+CarlosMiguelMartin@users.noreply.github.com>
2023-05-13 12:43:07 +0200

    Update Vector2D.h

--- AJEZ-main/Ajedrez2/Vector2D.h
+++ AJEZ-main/Ajedrez2/Vector2D.h
@@ -4,6 +4,7 @@ class Vector2D {
 
 public:
 	float x, y;
-	Vector2D(float x, float y):x(x),y(y){}
+	Vector2D(float xv, float yv):x(xv),y(yv){}
+	Vector2D operator - (Vector2D v);
 	Vector2D() :x(0), y(0){}
 };

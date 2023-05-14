8a4fdcd24f1f5a9c85f096bb04d83d1196dbbbc1
Alejandro de la Fuente Arranz <50597332+alejandrodelafuentearranz@users.noreply.github.com>
2023-05-14 23:37:34 +0200

    Add files via upload

---
+++ Ajedrez2/Movimiento.cpp
@@ -0,0 +1,6 @@
+#include"Movimiento.h"
+
+void Movimiento::insertarMov(const Vector2D& coord) {
+	lista_mov.push_back(coord);  //Añade nuevo movimiento. Se utilizará en el movimiento de las piezas
+
+}

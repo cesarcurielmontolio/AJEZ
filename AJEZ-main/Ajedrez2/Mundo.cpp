e9cec022994c38383da390cfa2148710f23b0a4c
cesarcurielmontolio <47853349+cesarcurielmontolio@users.noreply.github.com>
2023-05-15 18:26:17 +0200

    Update Mundo.cpp
    AÑADIDIO PUNTO DE VISTA DE LA PANTALLA.

--- AJEZ-main/Ajedrez2/Mundo.cpp
+++ AJEZ-main/Ajedrez2/Mundo.cpp
@@ -8,6 +8,9 @@ Mundo::~Mundo() {
 }
 
 void Mundo::Dibuja() {
+	gluLookAt(x_ojo, y_ojo, z_ojo,  // posicion del ojo
+		x_ojo, y_ojo, 0.0,      // hacia que punto mira  (0,0,0) 
+		0.0, 1.0, 0.0);      // definimos hacia arriba (eje Y) 
 	b.draw(); 
 }
 

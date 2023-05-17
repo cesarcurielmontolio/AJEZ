448d75f47dc5b4fd65ffc755de256108d38c0170
Alejandro de la Fuente Arranz <50597332+alejandrodelafuentearranz@users.noreply.github.com>
2023-05-17 18:26:12 +0200

    Add files via upload

--- AJEZ-main/Ajedrez2/Vector2D.cpp
+++ AJEZ-main/Ajedrez2/Vector2D.cpp
@@ -1,6 +1,11 @@
 #include "Vector2D.h"
 #include <math.h>
 
+Vector2D::Vector2D(int x, int y) {
+	this->x = x;
+	this->y = y;
+}
+
 Vector2D Vector2D::operator - (Vector2D v) {
 	Vector2D res;
 	res.x = x - v.x;
@@ -12,4 +17,18 @@ Vector2D Vector2D::operator + (Vector2D v) {
 	res.x = x + v.x;
 	res.y = y + v.y;
 	return res;
+}
+
+bool Vector2D::operator==(const Vector2D& v) {
+	return (this->x == v.x && this->y == v.y);
+};
+
+bool Vector2D::operator!=(const Vector2D& v) {
+	return !operator == (v);
+}
+bool Vector2D::operator < (const Vector2D& v) {
+	return (this->x < v.x && this->y < v.y);
+}
+bool Vector2D::operator > (const Vector2D& v) {
+	return !operator < (v);
 }
--- AJEZ-main/Ajedrez2/Vector2D.h
+++ AJEZ-main/Ajedrez2/Vector2D.h
@@ -4,10 +4,16 @@ class Vector2D {
 
 public:
 	int x, y;
-	Vector2D(int x, int y):x(x),y(y){}
+	Vector2D(int x, int y);
 	Vector2D() :x(0), y(0){}
 
 	Vector2D operator - (Vector2D); // resta de vectores
 	Vector2D operator + (Vector2D); // suma de vectores
+	
+	//Sobrecarga de operadores
+	bool operator==(const Vector2D& v);
+	bool operator!=(const Vector2D& v);
+	bool operator<(const Vector2D& v);
+	bool operator>(const Vector2D& v);
 	
 };

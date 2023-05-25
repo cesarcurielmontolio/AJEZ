6717661b92dbc30286bc430313b70259da68141e
Alejandro de la Fuente Arranz <50597332+alejandrodelafuentearranz@users.noreply.github.com>
2023-05-26 00:10:24 +0200

    Update Reina.h

--- AJEZ-main/Ajedrez2/Reina.h
+++ AJEZ-main/Ajedrez2/Reina.h
@@ -1,17 +1,21 @@
-#pragma once
 #include"Pieza.h"
 
 class Reina : public Pieza {
 
 public:
 
-	Reina(color c, Vector2D p) {
-		type = REINA;
+	Reina(color c,tipo t, Vector2D p) {
+		type = t;
 		colour = c;
 		posicion = p;
 
 	}
+
 	void dibujar(); //Derivada de func virtual en Pieza
-	bool movimiento(Vector2D pos_ini, Vector2D pos_fin, color c_destino);
-	Movimiento getMov(Vector2D pos_ini, Vector2D pos_fin);
+	void dibujarSW(); // dibujar piezas star wars 
+	bool movimiento(Vector2D pos_ini, Vector2D pos_fin);
+	bool movimiento2(Vector2D pos_ini, Vector2D pos_fin, color c);
+	color getCol() const;
+	char getTipo();
+
 };

6413a70eedadb8a22dbbb042e0f059a8e8c47066
Alejandro de la Fuente Arranz <50597332+alejandrodelafuentearranz@users.noreply.github.com>
2023-05-26 00:09:51 +0200

    Update Pieza.cpp

--- AJEZ-main/Ajedrez2/Pieza.cpp
+++ AJEZ-main/Ajedrez2/Pieza.cpp
@@ -1,16 +1,22 @@
 #include "Pieza.h"
-Vector2D Pieza::getPos() {
+
+Vector2D Pieza::getPos()
+{
 	return posicion;
 }
-int Pieza::getCoordX() {
+
+int Pieza::getCoordX()
+{
 	return posicion.x;
 }
-int Pieza::getCoordY() {
+
+int Pieza::getCoordY()
+{
 	return posicion.y;
 }
-void Pieza::setPos(Vector2D p) {
-	posicion = p;
+
+void Pieza::setPos(Vector2D v)
+{ 
+	posicion = v;
 }
-color Pieza::getCol() const {
-	return colour;
-}
+

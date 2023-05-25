d1bbb6ba230ea72d4026f07604202cff8a00e829
Alejandro de la Fuente Arranz <50597332+alejandrodelafuentearranz@users.noreply.github.com>
2023-05-25 23:55:41 +0200

    Update Vector2D.cpp

--- AJEZ-main/Ajedrez2/Vector2D.cpp
+++ AJEZ-main/Ajedrez2/Vector2D.cpp
@@ -1,34 +1,52 @@
 #include "Vector2D.h"
 #include <math.h>
 
-Vector2D::Vector2D(int x, int y) {
-	this->x = x;
-	this->y = y;
+Vector2D::Vector2D(float xv, float yv)
+{
+	x = xv;
+	y = yv;
 }
-
-Vector2D Vector2D::operator - (Vector2D v) {
+float Vector2D::modulo()
+{
+	return (float)sqrt(x * x + y * y);
+}
+float Vector2D::argumento()
+{
+	return (float)atan2(y, x);
+}
+Vector2D Vector2D::unitario()
+{
+	Vector2D retorno(x, y);
+	float mod = modulo();
+	if (mod > 0.00001)
+	{
+		retorno.x /= mod;
+		retorno.y /= mod;
+	}
+	return retorno;
+}
+Vector2D Vector2D::operator - (Vector2D v)
+{
 	Vector2D res;
 	res.x = x - v.x;
 	res.y = y - v.y;
 	return res;
 }
-Vector2D Vector2D::operator + (Vector2D v) {
+Vector2D Vector2D::operator + (Vector2D v)
+{
 	Vector2D res;
 	res.x = x + v.x;
 	res.y = y + v.y;
 	return res;
 }
-
-bool Vector2D::operator==(const Vector2D& v) {
-	return (this->x == v.x && this->y == v.y);
-};
-
-bool Vector2D::operator!=(const Vector2D& v) {
-	return !operator == (v);
+float Vector2D::operator * (Vector2D v) {
+	float a = x * v.x;
+	float b = y * v.y;
+	return (a + b);
 }
-bool Vector2D::operator < (const Vector2D& v) {
-	return (this->x < v.x && this->y < v.y);
-}
-bool Vector2D::operator > (const Vector2D& v) {
-	return !operator < (v);
+Vector2D Vector2D::operator * (float i) {
+	Vector2D res;
+	res.x = x * i;
+	res.y = y * i;
+	return res;
 }

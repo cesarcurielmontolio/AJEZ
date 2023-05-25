66494716677f69b04057b8e68445cc8f8bccc47c
Alejandro de la Fuente Arranz <50597332+alejandrodelafuentearranz@users.noreply.github.com>
2023-05-25 23:59:17 +0200

    Update Alfil.h

--- AJEZ-main/Ajedrez2/Alfil.h
+++ AJEZ-main/Ajedrez2/Alfil.h
@@ -5,13 +5,16 @@ class Alfil : public Pieza {
 
 public:
 
-	Alfil(color c, Vector2D p) {
+	Alfil(color c,tipo t, Vector2D p) {
 		type = ALFIL;
 		colour = c;
 		posicion = p;
 
 	}
 	void dibujar(); //Derivada de func virtual en Pieza
-	bool movimiento(Vector2D pos_ini, Vector2D pos_fin, color c_destino));
-	Movimiento getMov(Vector2D pos_ini, Vector2D pos_fin);
+	void dibujarSW(); //Dibujo temático
+	bool movimiento(Vector2D pos_ini, Vector2D pos_fin);
+	bool movimiento2(Vector2D pos_ini, Vector2D pos_fin, color c); //Movimiento con color incluido en el argumento
+	color getCol() const;
+	char getTipo();
 };

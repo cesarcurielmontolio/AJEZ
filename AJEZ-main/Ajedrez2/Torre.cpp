d3b43512e87cdaa49d54a5a3900e672d10c998d5
Alejandro de la Fuente Arranz <50597332+alejandrodelafuentearranz@users.noreply.github.com>
2023-05-26 00:13:12 +0200

    Update Torre.cpp

--- AJEZ-main/Ajedrez2/Torre.cpp
+++ AJEZ-main/Ajedrez2/Torre.cpp
@@ -1,14 +1,79 @@
 #include"Torre.h"
+
+
 void Torre::dibujar() {
+	
+	// std::cout << "Hola" << endl; 
+
+	if (Pieza::colour == 0) {
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, ETSIDI::getTexture("imagenes/torreblanca.png").id);
+	glDisable(GL_LIGHTING);
+	
+	}
+	else if (Torre::colour == 1) {
+		glEnable(GL_TEXTURE_2D);
+		glBindTexture(GL_TEXTURE_2D, ETSIDI::getTexture("imagenes/torrenegra.png").id);
+		glDisable(GL_LIGHTING);
+		
+	}
+	
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glBegin(GL_POLYGON);
+	glColor3f(1, 1, 1);
+	glTexCoord2d(0, 1); glVertex3f(posicion.x, posicion.y +1 , 0.1);
+	glTexCoord2d(1, 1); glVertex3f(posicion.x +1, posicion.y +1 , 0.1);
+	glTexCoord2d(1, 0); glVertex3f(posicion.x +1, posicion.y ,0.1);
+	glTexCoord2d(0, 0); glVertex3f(posicion.x  , posicion.y, 0.1);
+
+
+	glEnd();
+	glDisable(GL_BLEND);
+	glEnable(GL_LIGHTING);
+	glDisable(GL_TEXTURE_2D);
+	
+	
 
 }
-bool Torre::movimiento(Vector2D pos_ini, Vector2D pos_fin, color c_destino) {
+void Torre::dibujarSW()
+{
+	if (Pieza::colour == 0) {
+		glEnable(GL_TEXTURE_2D);
+		glBindTexture(GL_TEXTURE_2D, ETSIDI::getTexture("imagenesSW/torreblanca.png").id);
+		glDisable(GL_LIGHTING);
 
-	//Movimientos verticales (eje y, no cambia posición en x)
+	}
+	else if (Pieza::colour == 1) {
+		glEnable(GL_TEXTURE_2D);
+		glBindTexture(GL_TEXTURE_2D, ETSIDI::getTexture("imagenesSW/torrenegra.png").id);
+		glDisable(GL_LIGHTING);
+
+	}
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glBegin(GL_POLYGON);
+	glColor3f(1, 1, 1);
+	glTexCoord2d(0, 1); glVertex3f(posicion.x, posicion.y + 1, 0.1);
+	glTexCoord2d(1, 1); glVertex3f(posicion.x + 1, posicion.y + 1, 0.1);
+	glTexCoord2d(1, 0); glVertex3f(posicion.x + 1, posicion.y, 0.1);
+	glTexCoord2d(0, 0); glVertex3f(posicion.x, posicion.y, 0.1);
+
+
+
+	glEnd();
+	glDisable(GL_BLEND);
+	glEnable(GL_LIGHTING);
+	glDisable(GL_TEXTURE_2D);
+}
+bool Torre::movimiento(Vector2D pos_ini, Vector2D pos_fin) {
+
+	//Movimientos verticales (eje y, no cambia posiciÃ³n en x)
 	if ((pos_fin.x == pos_ini.x) && (pos_fin.y != pos_ini.y)) { //Movimiento ascendente o descendente
 		return true;
 	}
-		
+
 	//Movimientos horizontales (eje x, no cambia pos en y)
 	else if ((pos_fin.y == pos_ini.y) && (pos_fin.x != pos_ini.x)) {  //Desplazamiento lateral
 		return true;
@@ -19,38 +84,32 @@ bool Torre::movimiento(Vector2D pos_ini, Vector2D pos_fin, color c_destino) {
 
 }
 
-Movimiento Torre::getMov(Vector2D pos_ini, Vector2D pos_fin) {
-	Movimiento mov;
-	Vector2D desp(0, 0); //Inicializamos el desplazamiento a 0 en ambos sentidos
-	Vector2D origen = pos_ini; //Guardamos en origen la posición inicial
-
-	if (pos_fin.x != pos_ini.x) { //Mov eje x
-		if (pos_fin.x > pos_ini.x) {
-			desp.x = 1; //Desplazamiento positivo en el eje x de la matriz
-		}
-		else {
-			desp.x = -1;
-		}
-	}
-	else if (pos_fin.y != pos_ini.y) { //Mov eje y
-		if (pos_fin.y > pos_ini.y) {
-			desp.y = 1; //Desplazamiento positivo en el eje y de la matriz
-		}
-		else {
-			desp.y = -1;
-		}
+bool Torre::movimiento2(Vector2D pos_ini, Vector2D pos_fin, color c) {
+	//Movimientos verticales (eje y, no cambia posiciÃ³n en x)
+	if ((pos_fin.x == pos_ini.x) && (pos_fin.y != pos_ini.y)) { //Movimiento ascendente o descendente
+		return true;
 	}
 
-	//Se incrementa o decrementa según proceda hasta que llegue a la posición final
-	while (origen != pos_fin) { //Hay que hacer la sobrecarga del operador 
-		if (pos_fin.x != origen.x) {
-			origen.x += desp.x;
-		}
-		else if (pos_fin.y != origen.y) {
-			origen.y += desp.y;
-		}
-		mov.insertarMov(origen);
+	//Movimientos horizontales (eje x, no cambia pos en y)
+	else if ((pos_fin.y == pos_ini.y) && (pos_fin.x != pos_ini.x)) {  //Desplazamiento lateral
+		return true;
 	}
-	return mov;
+	else {
+		return false;
+	}
+}
+
+
+color Torre::getCol() const{
+	if (colour == color::BLANCA) {
+		return color::BLANCA;
+	}
+	else if (colour == color::NEGRA) {
+		return color::NEGRA;
+	}
+
+}
+char Torre::getTipo() {
+	return 'T';
 
 }

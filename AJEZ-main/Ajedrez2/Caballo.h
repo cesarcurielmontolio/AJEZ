2117cd7f3e43c7657691b8f52beb1240947cb6d4
Alejandro de la Fuente Arranz <50597332+alejandrodelafuentearranz@users.noreply.github.com>
2023-05-26 00:00:30 +0200

    Update Caballo.h

--- AJEZ-main/Ajedrez2/Caballo.h
+++ AJEZ-main/Ajedrez2/Caballo.h
@@ -1,17 +1,21 @@
 #pragma once
-#include"Pieza.h"
-
+#include "Pieza.h"
 class Caballo : public Pieza {
 
-public:
 
-	Caballo(color c, Vector2D p) {
-		type = CABALLO;
-		colour = c;
+
+public: 
+	Caballo(color c, tipo t, Vector2D p) {
+		colour = c; 
+		type = t;
 		posicion = p;
-
 	}
+
 	void dibujar(); //Derivada de func virtual en Pieza
-	bool movimiento(Vector2D pos_ini, Vector2D pos_fin, color c_destino);
-	Movimiento getMov(Vector2D pos_ini, Vector2D pos_fin);
+	void dibujarSW(); //Dibujo temático
+	//virtual void dibuja(Vector2D p) = 0; 
+	bool movimiento(Vector2D pos_ini, Vector2D pos_fin);
+	bool movimiento2(Vector2D pos_ini, Vector2D pos_fin, color c); //Función de movimiennto
+	color getCol() const;
+	char getTipo();
 };

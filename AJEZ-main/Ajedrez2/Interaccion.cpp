e4c6755639e3fdbe5cfd6bd9c7471090f284078d
Alejandro de la Fuente Arranz <50597332+alejandrodelafuentearranz@users.noreply.github.com>
2023-05-26 19:19:49 +0200

    Update Interaccion.cpp

--- AJEZ-main/Ajedrez2/Interaccion.cpp
+++ AJEZ-main/Ajedrez2/Interaccion.cpp
@@ -1,47 +1,6 @@
 #include "Interaccion.h"
 #include "Tablero.h"
 
-bool interaccion::selC(CaballoN& c, Selector s)
-{
-
-	Vector2D dif = s.getPos() - c.getPos();
-	float u = dif.modulo();
-	if (u <= 2.0)
-		return true;
-	else return false;
-
-
-
-}
-
-bool interaccion::sitioCCorrecto(Selector& s, Vector2D p)
-{
-	Vector2D k;
-	if (s.getPos().x == p.x + 6 && s.getPos().y == p.y + 12)
-		return true;
-	else return false;
-
-	/*
-	for (float i = 6; i <= 12; i + 6)
-	{
-		for (float j = 12; j >= 0; j - 6)
-		{
-
-			if (c.getPos().x == p.x + i && c.getPos().y == p.y + j)
-				return true;
-			else if ((c.getPos().x == p.x + i) && (c.getPos().y == p.y - j))
-				return true;
-			else if (c.getPos().x == p.x - i && c.getPos().y == p.y + j)
-				return true;
-			else if ((c.getPos().x == p.x - i) && (p.y - j))
-				return true;
-			else
-				return false;
-		}
-	}
-	*/
-
-}
 
 bool interaccion::recorridoTorre(Tablero& t, Vector2D pos_ini, Vector2D pos_fin)
 {
@@ -184,33 +143,4 @@ bool interaccion::comerPeon(Tablero& t, Vector2D pos_ini, Vector2D pos_fin)
 }
 
 
-/*
-bool interaccion::CaballoCorrecto(CaballoN& c, Vector2D p)
-{
-	/*if (c.getPos().x == p.x + 6 && c.getPos().y == p.y + 12)
-		return true;
-	else return false;
-
-	for (float i = 6; i <= 12; i + 6)
-	{
-		for (float j = 12; j >= 0; j - 6)
-		{
-
-			if (c.getPos().x == p.x + i && c.getPos().y == p.y + j)
-				return true;
-			else if ((c.getPos().x == p.x + i) && (c.getPos().y == p.y - j))
-				return true;
-			else if (c.getPos().x == p.x - i && c.getPos().y == p.y + j)
-				return true;
-			else if ((c.getPos().x == p.x - i) && (p.y - j))
-				return true;
-			else
-				return false;
-		}
-
-	}
-
-}
-
-*/
 
